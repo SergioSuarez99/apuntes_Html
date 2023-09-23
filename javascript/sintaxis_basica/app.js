8bb0486073d281b0f3b29791909c22afd65ac290
Sergio Suarez <sergio.990101@gmail.com>
2023-09-22 21:49:07 -0500

    primer Hola Mundo en JS

---
+++ javascript/sintaxis_basica/app.js
@@ -0,0 +1,20 @@
+// Variables globales se definen con VAR
+var Variable="Hola esta es mi Variable"
+console.log(Variable)
+Variable="Segundo nombre de la Variable"
+console.log("hola estta es mi primera App")
+Variable=variableDos = 60
+console.log(variableDos)
+{
+    let variableLocal ="Variable local"
+    console.log(variableLocal)
+    var variableGloballet = "Variable local dentro del ciclo"
+}
+
+let numero =13;
+// para los boleanos tenemos dos posibles valores true y false
+let booleano = false
+let caracteres ="Hola Mundo"
+
+// CONSTANTES
+const pi=3.1416
